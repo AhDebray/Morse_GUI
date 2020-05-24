50041d0f6f719a9c404d8344262144ab327c5988
AhDebray <52772932+AhDebray@users.noreply.github.com>
2020-05-24 11:18:35 +1000

    Update ComplexGUI.py

--- ComplexGUI.py
+++ ComplexGUI.py
@@ -16,410 +16,44 @@ word = StringVar()
 win.geometry('450x450+500+300')
 win.title('Morse Code')
 
-def mhello():
-    mtext = word.get()
-    mtext = to_morse_code(mtext)
-    mlabel2 = Label(win, text = mtext).pack()
+def Mcode():
+    try:
+        mtext = word.get()
+        if len(mtext) > 12:
+            mlabel = Label(win, text = 'Word is too long').pack()
+        else:
+            for i in mtext:
+                for j in code[i.lower()]:
+                    if j == '-':
+                        dash()
+                        mlabel = Label(win, text = j).pack()
+                    elif j == '.':
+                        dot()
+                        mlabel = Label(win, text = j).pack()
+                    else:
+                        time.sleep(0.5)
+                time.sleep(0.5)
+                
+    except KeyboardInterrupt:
+        GPIO.cleanup()
     
-    for i in mtext:
-        if(i == '1'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '2'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '3'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-        
-        if(i == '4'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-        
-        if(i == '5'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-        
-        if(i == '6'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '7'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-        
-        if(i == '8'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '9'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '10'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '11'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '12'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-
-        if(i == '13'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '14'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '15'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '16'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-
-        if(i == '17'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '18'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-
-        if(i == '19'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '20'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-
-        if(i == '21'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-
-        if(i == '22'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-
-        if(i == '23'):
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-
-        if(i == '24'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-            
-        if(i == '25'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(1)
-
-        if(i == '26'):
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.6)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(0.3)
-            led.on()
-            time.sleep(0.3)
-            led.off()
-            time.sleep(1)
-        
-            
-        return
-
-def to_morse_code(word):
-    code = {' ': '|', 'a': '1', 'b': '2', 'c': '3', 'd': '4', 'e': '5', 'f': '6', 'g': '7',
-            'h': '8', 'i': '9', 'j': '10', 'k': '11', 'l': '12', 'm': '13', 'n': '14', 'o': '15',
-            'p': '16', 'q': '17', 'r': '18', 's': '19', 't': '20', 'u': '21', 'v': '22', 'w': '23',
-            'x': '24', 'y': '25', 'z': '26'}
+def dot():
+        led.on()
+        time.sleep(0.3)
+        led.off()
+        time.sleep(0.3)
+def dash():
+        led.on()
+        time.sleep(0.6)
+        led.off()
+        time.sleep(0.6)
     
-    morse_code = ""
+
+
+code = {' ': '|', 'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....',
+        'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-',
+        'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..'}
     
-    for x in word:
-        morse_code += code[x.lower()]
-    
-    return morse_code
 
-mlabel = Label(win, text = 'My Label').pack()
-
-mbutton = Button(win, text = 'OK', command = mhello, fg = 'red', bg= 'blue').pack()
-
+mbutton = Button(win, text = 'Execute', command = Mcode, fg = 'red', bg= 'blue').pack()
 textEntry = Entry(win, textvariable = word).pack()
